e12c18d0018b901dd3ecb7c97b8c960447c8326a
Ahmed Zakaria Namane <84190793+ZikoDz23@users.noreply.github.com>
2025-01-15 20:44:25 +0100

    Update Info.tsx

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -9,7 +9,7 @@ const Info: React.FC = () => {
             <h1 className="text-2xl text-blue-500 font-bold text-center font-press press-start-2p-regular">Coming soon ... </h1>
         </div>
         <div className="flex flex-col items-center gap-5 w-full">
-            <img src="assets\globeterra.png" alt="" className=" h-96 w-96" />
+            <img src="src/assets/globeterra.png" alt="" className=" h-96 w-96" />
             <div className="flex flex-row items-center gap-5 text-xl font-bold text-white">
             <h1 className="text-2xl">join our Social networks : </h1>
             <a href="https://x.com/GlobeTerraCoin?t=mI_JkHsDurgOY1QFafb_8w&s=35" className="px-1 py-2 border border-blue-400 rounded-lg text-blue-400 hover:scale-105">Twitter</a>
